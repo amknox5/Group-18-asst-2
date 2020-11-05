d34a2811890e9dbf651e66646b4f7613269f5a80
amknox5 <aknox@umd.edu>
2020-11-05 14:14:50 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,17 +1,3 @@
-function range(int) {
-    const arr = [];
-    for (let i = 0; i < int; i += 1) {
-        arr.push(i);
-    }
-    return arr;
-}
-
-function getRandomIntInclusive(min, max) {
-    const min1 = Math.ceil(min);
-    const max1 = Math.floor(max);
-    return Math.floor(Math.random() * (max1 - min1 + 1) + min1);
-}
-
 function changeDataShape(array) {
     return array.reduce((list, item, i) => {
         const findCat = list.find((findItem) => findItem.label === item.category);
@@ -26,27 +12,27 @@ function changeDataShape(array) {
 }
 
 function manipulateAndBind(incomingArray) {
-    const arrayOfTenItems = range(10);
-    const randomRestaurantArray = arrayOfTenItems.map((item) => {
-        const which = getRandomIntInclusive(0, incomingArray.length);
-        const restaurant = incomingArray[which];
+    const arr = [] ;
+    const api = loadData()
+    arr.push(api);
+
+    const randomRestaurantArray = arr.map((item) => {
+        const restaurant = incomingArray;
         return restaurant;
     });
-
+    console.log(randomRestaurantArray)
     sessionStorage.setItem('shortRestaurantList', JSON.stringify(randomRestaurantArray));
 
     const div = document.createElement('div');
     div.innerHTML = `<h2>What we have</h2> <br />${JSON.stringify(randomRestaurantArray[0])}<br/><br/>`;
     $('.flex-outer').append(div);
 
-    const newDataShape = changeDataShape(randomRestaurantArray);
-
     const div2 = document.createElement('div');
     const obj = {
-        name: randomRestaurantArray[0].name,
-        address: randomRestaurantArray[0].address_line_1,
-        label: randomRestaurantArray[0].category,
-        zip: randomRestaurantArray[0].zip
+        name: randomRestaurantArray[0][0].name,
+        address: randomRestaurantArray[0][0].address_line_1,
+        label: randomRestaurantArray[0][0].category,
+        zip: randomRestaurantArray[0][0].zip
     };
     div2.innerHTML = `<h2>What we want</h2> <br/> <h4>A category, how many things are in the category</h4><pre><code class="language-javascript">${JSON.stringify(obj)}<br/><br/>`;
 
@@ -57,6 +43,7 @@ async function loadData() {
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const json = await data.json();
     return json;
+    
 }
 
 async function mainThread() {
@@ -68,4 +55,4 @@ async function mainThread() {
     console.table(restaurantData);
 }
 
-window.onload = mainThread;
+window.onload = mainThread; 
